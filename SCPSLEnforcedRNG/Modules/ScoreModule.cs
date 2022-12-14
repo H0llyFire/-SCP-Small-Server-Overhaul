a62c5410cb31e91c2d1d079ce8078615d9f5fe07
H0llyFire <H0llyFire@seznam.cz>
2022-12-14 17:30:11 +0100

    New Database structure Implemented, new stats Module created

---
+++ SCPSLEnforcedRNG/Modules/ScoreModule.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPSLEnforcedRNG.Modules
+{
+    public class Score : BaseModule
+    {
+        /* Score Calcs?
+         * Base Score 100pts
+         
+         * 100% of total Score if win as the same team you spawn as
+         
+         * [CUTS]
+         * cut of 25% if you respawn as the same team you spawned as and win (halves after each trigger 25 => 12.5 => 6.25 =>...)
+         * cut of 40% if you respawn as a different team and win (halves after each trigger)
+         * cut of 50% if you die, but your team wins (last team you lived as)
+         * cut of 75% if you lose but still live (Chaos/SCP)
+         * cut of 100% if you die and your team loses (last team you lived as)
+         * cut of 25% if stalemate 
+         
+         * [ADDITIONAL POINTS]
+         * + 20 pts for killing an armed enemy
+         * + 1 pts for killing Kritik
+         * + 50 pts for killing SCP
+         * + 25 pts for arming a nuke that explodes
+         * + 20 pts for escaping
+         * + % of damage dealt to scp (max 10 per SCP => 100% dealt)
+         * + 5 pts for actually activating a generator as MTF-Friendly role
+         * + 5 pts for deactivating a generator as Chaos-Friendly role
+         * + 10 pts for executing SCP 079
+         * ...
+         
+         * [SUBTRACTED POINTS]
+         * - 10 pts for each minute you stay in the same fucking room (leaving for a few secs and going back is enough for a timer reset)
+         * - 1 pts for killing H0lly lulw
+         * - 40 pts to all non-SCP-Friendly roles if SCP 106 is terminated by Femur Breaker
+         * ...
+         */
+
+        //-------------------------------------------------------------------------------
+        //Override
+        public override string ModuleName { get { return "Score"; } }
+
+        public override void Activate()
+        {
+            SynapseController.Server.Events.Round.RoundEndEvent += CountScore;
+        }
+        public override void SetUpRound()
+        {
+        }
+
+        //-------------------------------------------------------------------------------
+        //Main
+
+        //-------------------------------------------------------------------------------
+        //Events
+        private static void CountScore()
+        {
+
+        }
+    }
+}
---
+++ SCPSLEnforcedRNG/NewDataBase.cs
@@ -0,0 +1,107 @@
+﻿using Synapse.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPSLEnforcedRNG
+{
+    public class ServerDB : IDatabaseEntity
+    {
+        public int Id { get; set; }
+        public int GetId() => Id;
+
+        public string? Port { get; set; }
+    }
+    public class ServerRepo : Repository<ServerDB>
+    {
+        public ServerDB FindById(int id) => Get(LiteDB.Query.EQ("Id", id));
+        public ServerDB FindByPort(string port) => Get(LiteDB.Query.EQ("Port", port));
+    }
+
+    public class PlayerDB : IDatabaseEntity
+    {
+        public int Id { get; set; }
+        public int ServerId { get; set; }
+        public int GetId() => Id;
+
+        public string? Name { get; set; }
+        public string? SteamId { get; set; }
+        public uint NotSCP          { get; set; } = 0;
+        public uint NotGuard        { get; set; } = 0;
+        public uint NotDboi         { get; set; } = 0;
+        public uint NotScientist    { get; set; } = 0;
+        public uint NotPC           { get; set; } = 0;
+        public ushort PrefferedRole { get; set; }
+    }
+    public class PlayerRepo : Repository<PlayerDB>
+    {
+        public PlayerDB FindById(int id) => Get(LiteDB.Query.EQ("Id", id));
+        public PlayerDB FindBySteamId(string id) => Get(LiteDB.Query.EQ("SteamId", id));
+        public PlayerDB FindByName(string name) => Get(LiteDB.Query.EQ("Name", name));
+
+    }
+
+    public class RoundDB : IDatabaseEntity
+    {
+        public int Id { get; set; }
+        public int ServerId { get; set; }
+        public int GetId() => Id;
+
+        public int Session { get; set; }
+        public int Round { get; set; }
+
+        public int TimesRonDiedToDoggoInCP { get; set; } = 0;
+        public int EscapedDClass { get; set; } = 0;
+        public int EscapedScientist { get; set; } = 0;
+        public float TotalDamageDealt { get; set; } = 0;
+        public int TotalMTFSpawns { get; set; } = 0;
+        public int TotalCISpawns { get; set; } = 0;
+        public int DoorInteracts { get; set; } = 0;
+        public int TeslaGateKills { get; set; } = 0;
+        public float HIDUsage { get; set; } = 0;
+
+    }
+    public class RoundRepo : Repository<RoundDB>
+    {
+        public RoundDB FindById(int id) => Get(LiteDB.Query.EQ("Id", id));
+        public RoundDB FindByRound(int round, int session)
+            => Get(LiteDB.Query.And("$.Round = " + round, "$.Session = " + session));
+        public RoundDB FindLastRound()
+        {
+            try { return Query(queryable => queryable.ToList()).Last(); }
+            catch { return null; }
+        }
+    }
+
+    public class PlayerRoundDB : IDatabaseEntity
+    {
+        public int Id { get; set; }
+        public int RoundId { get; set; }
+        public int PlayerId { get; set; }
+        public int GetId() => Id;
+
+        public float DamageDealt { get; set; } = 0f;
+        public float SCPDamageDealt { get; set; } = 0f;
+        public int TotalKills { get; set; } = 0; 
+        public float DamageHealed { get; set; } = 0; 
+        public int SCPItemsUsed { get; set; } = 0;
+        public int TimesRespawned { get; set; } = 0; 
+        public float EscapeTime { get; set; } = 0f;
+        public int DoorInteracts { get; set; } = 0; 
+        public int ShotsFired { get; set; } = 0; 
+        public ItemType HighestKeycardHeld { get; set; } = ItemType.None; 
+        public int GeneratorsActivated { get; set; } = 0; 
+        public int GeneratorsStopped { get; set; } = 0; 
+        public int CoinFlips { get; set; } = 0; 
+
+    }
+    public class PlayerRoundRepo : Repository<PlayerRoundDB>
+    {
+        public PlayerRoundDB FindById(int id) => Get(LiteDB.Query.EQ("Id", id));
+        public PlayerRoundDB FindByRoundPlayer(int roundId, int playerId)
+            => Get(LiteDB.Query.And("RoundId = " + roundId, "PlayerId = " + playerId));
+
+    }
+}
